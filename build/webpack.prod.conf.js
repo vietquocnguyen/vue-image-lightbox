3e44f5617849198c7d9163fc3b8059de6ee559d2
Nguyen Thien Dzung <dungtoanhoc@yahoo.com.vn>
2017-05-23 10:44:19 +0700

    feat(page): add index for pages

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -4,18 +4,22 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 const path = require('path')
 
-config.output.filename = 'vue-image-lightbox.min.js'
-
-config.entry = path.resolve(__dirname, '../src/components/Lightbox.vue')
-
 config.devtool = '#source-map'
 
+// config.module.rules.push({
+//   test: /\.css$/,
+//   use: ExtractTextPlugin.extract({
+//     fallback: "style-loader",
+//     use: "css-loader",
+//   }),
+// })
+
 config.module.rules.push({
-  test: /\.css$/,
-  use: ExtractTextPlugin.extract({
-    fallback: "style-loader",
-    use: "css-loader",
-  }),
+  test: /\.css/,
+  use: [
+    { loader: 'style-loader' },
+    { loader: 'css-loader' },
+  ],
 })
 
 config.plugins = (config.plugins || []).concat([
@@ -23,9 +27,9 @@ config.plugins = (config.plugins || []).concat([
     'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
   }),
 
-  new ExtractTextPlugin({
-    filename: 'vue-image-lightbox.min.css',
-  }),
+  // new ExtractTextPlugin({
+  //   filename: 'vue-image-lightbox.min.css',
+  // }),
 
   new UglifyJSPlugin({
     compress: {
